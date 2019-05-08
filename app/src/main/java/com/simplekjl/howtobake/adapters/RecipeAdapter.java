c48a4b146240178a48436d3f185325261318ecd5
Jose Luis Crisostomo <jl.cs606@gmail.com>
2019-05-08 14:09:00 -0700

    New stuff 2.0

--- app/src/main/java/com/simplekjl/howtobake/adapters/RecipeAdapter.java
+++ app/src/main/java/com/simplekjl/howtobake/adapters/RecipeAdapter.java
@@ -5,11 +5,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
-
 import androidx.annotation.NonNull;
 import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
-
 import com.simplekjl.howtobake.R;
 import com.simplekjl.howtobake.models.Recipe;
 import com.simplekjl.howtobake.utils.BakingImagesAssets;
